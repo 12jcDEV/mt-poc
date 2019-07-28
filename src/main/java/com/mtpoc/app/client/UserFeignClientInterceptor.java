e1f16d20fda8dcd0b5289f0486602054a20967b5
Jose Fabian <jcfabianwork@gmail.com>
2019-07-28 10:37:28 +0800

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/mtpoc/app/client/UserFeignClientInterceptor.java
@@ -0,0 +1,18 @@
+package com.mtpoc.app.client;
+
+import com.mtpoc.app.security.SecurityUtils;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFeignClientInterceptor implements RequestInterceptor {
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER = "Bearer";
+
+    @Override
+    public void apply(RequestTemplate template) {
+        SecurityUtils.getCurrentUserJWT()
+            .ifPresent(s -> template.header(AUTHORIZATION_HEADER,String.format("%s %s", BEARER, s)));
+    }
+}
---
+++ src/main/java/com/mtpoc/app/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.mtpoc.app.config.audit;
